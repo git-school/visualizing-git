c42a0e27825ed41c1eacd720eefbdfb27eba0310
dpatel19 <skamazin@gmail.com>
2020-04-18 15:21:23 -0400

    Removed debug prints

--- js/historyview.js
+++ js/historyview.js
@@ -530,9 +530,6 @@ define(['d3'], function() {
         .attr('width', this.width)
         .attr('height', this.isRemote ? this.height + 150 : this.height);
 
-      console.log(this.width);
-      console.log(this.height);
-
       if (this.isRemote) {
         svg.append('svg:text')
           .classed('remote-name-display', true)
